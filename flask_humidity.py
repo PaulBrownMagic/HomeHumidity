7d03499c3b7aefa38c65b93c58f5d2c3fa1cb948
PaulBrownMagic <pbrownpaul@gmail.com>
2018-01-05 23:14:28 +0000

    Live Graph Draft

--- flask_humidity.py
+++ flask_humidity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3.6
 """Serve Humidity information via Flask"""
 
-from flask import Flask
+from time import time
+from flask import Flask, render_template
 
 from redis_connection import RedisConnection
 
@@ -13,7 +14,14 @@ rconn = RedisConnection()
 def home_page():
    humidities = rconn.retrieve(10)
    humidity = round(sum(humidities)/len(humidities), 2)
-   return f"<h1>Humidity {humidity}%</h1>"
+   #return f"<h1>Humidity {humidity}%</h1>"
+   return render_template("index.html")
+
+@app.route("/data")
+def humidity_data():
+    humidity = rconn.retrieve(1)[0]
+    return f'[[{time()},{humidity}]]'
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
